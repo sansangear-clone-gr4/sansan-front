927b18b3ba8f7bcee84ec01599fad60b5b288650
오채운 <treechae38@naver.com>
2022-12-24 16:18:21 +0900

    산산기어 초안

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,71 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 function Form() {
-  return <div>난 작성컴포넌트</div>;
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+    imageFile: "",
+    price: "",
+    category: "",
+  });
+
+  // const dispatch = useDispatch();
+  console.log(post);
+  const OnSubmitHandler = () => {};
+
+  return (
+    <form onSubmit={OnSubmitHandler}>
+      <div>
+        <input
+          type="text"
+          placeholder="이름"
+          minLength="2"
+          onChange={(e) => {
+            const { value } = e.target;
+            setPost({ ...post, title: value });
+          }}
+        />
+        <select
+          name="category"
+          placeholder="카테고리"
+          onChange={(e) => {
+            const { value } = e.target;
+            setPost({ ...post, category: value });
+          }}
+        >
+          <option value="outer">Outer</option>
+          <option value="top">Top</option>
+          <option value="bottom">Bottom</option>
+          <option value="accessories">Accessories</option>
+        </select>
+      </div>
+      <div>
+        <textarea
+          row="50"
+          cols="80"
+          type="text"
+          id="content"
+          placeholder="내용"
+          onChange={(e) => {
+            const { value } = e.target;
+            setPost({ ...post, content: value });
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="가격"
+          onChange={(e) => {
+            const { value } = e.target;
+            setPost({ ...post, price: value });
+          }}
+        />
+      </div>
+      <button type="submit">ADD POST</button>
+    </form>
+  );
 }
 
 export default Form;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/config/configStore";
-import Main from "./pages/Main";
+// import store from "./redux/config/configStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
 
 // main(최종) ->
 
