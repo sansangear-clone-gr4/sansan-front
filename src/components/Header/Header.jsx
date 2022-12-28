763382f4fa38c2e7e99d00040170231b18b39833
lidieee <seb384@naver.com>
2022-12-28 13:11:51 +0900

    shop 페이지 수정 중

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,48 @@
-function Header() {
+import { useNavigate } from "react-router-dom";
+import "../../pages/reset.css"
+import "./style.css"
 
-  return <div>난헤더</div>;
+function Header() {
+  const navigate = useNavigate();
+
+  return <div>
+    <div className="headerwrap">
+      <nav className="gnb">
+        <ul className="clearfix">
+          <li onClick={() => navigate('/Shop')}>Shop</li>
+          <li><a href="">Archive</a></li>
+          <li><a href="">Vue</a></li>
+          <li><a href="">Stocklist</a></li>
+        </ul>
+      </nav>
+      <div className="logo">
+        <h1>
+          <a href="/">LOGO</a>
+        </h1>
+      </div>
+      <div className="topmenu">
+        <ul className="clearfix">
+          <li><a href="">Contact</a></li>
+          {!localStorage.getItem("id") ? (
+                <li onClick={() => navigate("/LogIn")} className="LogIn">
+                  Login
+                </li>
+              ) : (
+                <li
+                  onClick={() => {
+                    localStorage.removeItem("id");
+                    navigate("/LogIn");
+                  }}
+                  className="LogIn"
+                >
+                  Sign Out
+                </li>
+              )}
+          <li onClick={() => navigate("/Bucket")}>Cart (0)</li>
+        </ul>
+      </div>
+    </div>
+  </div>;
 
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import './style.css'
 
 function Main() {
   return (
     <>
       <Header />
+      <div className="mainpage"></div>
       <Footer />
     </>
   );
